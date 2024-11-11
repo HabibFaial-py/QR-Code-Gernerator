c1eca22e6cd090682d42b04e02d1c9247bae2793
Habib Faisal <habibfaisaljandira@gmail.com>
2024-11-11 17:28:38 +0500

    Add files via upload

---
+++ QR-Code_Gernerator.py
@@ -0,0 +1,19 @@
+import qrcode
+def qr_code(text,file_name):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_H,
+        box_size = 10,
+        border = 4,
+
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color = "#6aff9b",background_color = "black")
+    img.save(file_name)
+
+text = "https://github.com/dashboard"
+file_name = "githubcode2.png"
+
+qr_code("text","githubcode2.png")
+
